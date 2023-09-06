1a328d5d31222d6325dec2190af226cbd49b3a2e
mxolisngcobo <sbaningcobo2511525115@gmail.com>
2023-09-06 12:38:19 +0200

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,41 @@
+<h2 class="brand"><a href="index.php"> Fo<span style="color: red">rex</span>Briefly</a></h2>
+    
+ <nav class="navbar fixed-top navbar-expand-lg navbar-dark  fixed-top bg-color-main" style="background-color:#1d2330">
+
+      <div class="container">
+        
+      
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          
+          <ul class="navbar-nav ml-auto">
+          </li>
+                 <li class="nav-item">
+              <a class="nav-link" href="index.php"> <i class="fa fa-home"></i> Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="category.php?catid=24"> Forex</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="category.php?catid=25"> Prop Firms</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="category.php?catid=26"> Brokers</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="category.php?catid=27">  Wall Of Scammers</a>
+            </li>
+  
+             <li class="nav-item  w-f-u">
+              <a class="nav-link" href="Write-for-us.php" > <i class="fa fa-book"></i> Write for Us</a>
+            </li>
+  
+  
+          </ul>
+        </div>
+      </div>
+    </nav>
